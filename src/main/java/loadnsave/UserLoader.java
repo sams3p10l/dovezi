737d96d77253bbb935e8f2019e89f155cfbf5779
sams3p10l <mix996@gmail.com>
2019-01-19 04:52:16 +0100

    json parsers

--- src/main/java/loadnsave/UserLoader.java
+++ src/main/java/loadnsave/UserLoader.java
@@ -1,12 +1,20 @@
 package loadnsave;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import user_classes.Admin;
 import user_classes.Customer;
 import user_classes.Deliverer;
 import user_classes.User;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.json.simple.parser.JSONParser;
@@ -26,9 +34,52 @@ public class UserLoader //loading and parsing data from json
         loadDeliverers();
     }
 
-    private void loadCustomers()
+    private void loadCustomers() throws IOException, ParseException
     {
         JSONParser parser = new JSONParser();
+
+        try {
+
+            JSONArray jsonArray = (JSONArray) parser.parse
+                    (new FileReader("E:\\Misa\\Docs\\Web\\dovezi2\\web\\data"));
+
+            for (Object obj : jsonArray)
+            {
+                JSONObject jsonObject = (JSONObject) obj;
+
+                String username = (String) jsonObject.get("username");
+                String password = (String) jsonObject.get("password");
+                String firstName = (String) jsonObject.get("firstname");
+                String lastName = (String) jsonObject.get("lastname");
+                String role = (String) jsonObject.get("role"); //cemu?
+                String phone = (String) jsonObject.get("phone");
+                String email = (String) jsonObject.get("email");
+                String regDate = (String) jsonObject.get("regdate");
+                int points = Integer.parseInt((String) jsonObject.get("points"));
+                JSONArray userOrders = (JSONArray) jsonObject.get("orderList");
+                JSONArray userRestaurants = (JSONArray) jsonObject.get("restaurantList");
+
+                ArrayList<String> orderList = new ArrayList<String>();
+                for (Object userOrder : userOrders) {
+                    orderList.add((String) userOrder);
+                }
+
+                ArrayList<String> restaurantList = new ArrayList<String>();
+                for (Object userRestaurant : userRestaurants) {
+                    restaurantList.add((String) userRestaurant);
+                }
+
+                User userCustomer = new User(username, password, firstName, lastName, role, phone,
+                                                                email, regDate);
+                allUsers.put(userCustomer.getUsername(), userCustomer);
+
+                Customer newCustomer = new Customer(username, password, firstName, lastName, role, phone,
+                                                                email, orderList, restaurantList);
+                customers.add(newCustomer);
+
+            }
+
+        }
 
     }
 
