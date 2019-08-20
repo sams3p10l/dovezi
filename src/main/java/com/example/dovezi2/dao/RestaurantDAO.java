8539217c2580e3e3f6973aa69bded928d2a52063
sams3p10l <mix996@gmail.com>
2019-08-20 19:29:07 +0200

    restaurant loader done

--- src/main/java/com/example/dovezi2/dao/RestaurantDAO.java
+++ src/main/java/com/example/dovezi2/dao/RestaurantDAO.java
@@ -1,5 +1,8 @@
 package com.example.dovezi2.dao;
 
+import com.example.dovezi2.loadnsave.RestaurantLoader;
+
 public class RestaurantDAO
 {
+    private RestaurantLoader restaurantLoader = new RestaurantLoader();
 }
--- src/main/java/com/example/dovezi2/loadnsave/RestaurantLoader.java
+++ src/main/java/com/example/dovezi2/loadnsave/RestaurantLoader.java
@@ -1,13 +1,16 @@
 package com.example.dovezi2.loadnsave;
 
 import com.example.dovezi2.model_classes.Restaurant;
+import com.example.dovezi2.model_classes.enums;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,8 +32,78 @@ public class RestaurantLoader
     private void loadRestaurants() throws IOException, ParseException
     {
         JSONParser parser = new JSONParser();
-
         JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(path + "../../data/restaurants.json"));
+
+        for (Object restObject : jsonArray)
+        {
+            JSONObject obj = (JSONObject) restObject;
+
+            //String id = (String) obj.get("id");
+            String name = (String) obj.get("name");
+            String address = (String) obj.get("address");
+            enums.Categories category = restString2Enum((String) obj.get("category"));
+
+            JSONArray arrayOfFoods = (JSONArray) obj.get("listOfFoods");
+            ArrayList<String> foodList = new ArrayList<>();
+
+            for (Object foodObject : arrayOfFoods)
+            {
+                foodList.add(foodObject.toString());
+            }
+
+            JSONArray arrayOfDrinks = (JSONArray) obj.get("listOfDrinks");
+            ArrayList<String> drinkList = new ArrayList<>();
+
+            for (Object drinkObject : arrayOfDrinks)
+            {
+                drinkList.add(drinkObject.toString());
+            }
+
+            //boolean delete = (boolean)jsonObject.get("delete");
+
+            Restaurant newRestaurant = new Restaurant(name, address, category, foodList, drinkList);
+            allRestaurants.put(newRestaurant.getNaziv(), newRestaurant);
+
+        }
+    }
+
+    private enums.Categories restString2Enum(String rest)
+    {
+        switch (rest)
+        {
+            case "DOMACA_KUHINJA":
+            {
+                return enums.Categories.DOMACA_KUHINJA;
+                //break;
+            }
+            case "ROSTILJ":
+            {
+                return enums.Categories.ROSTILJ;
+            }
+            case "KINESKI_RESTORAN":
+            {
+                return enums.Categories.KINESKI_RESTORAN;
+            }
+            case "INDIJSKI_RESTORAN":
+            {
+                return enums.Categories.INDIJSKI_RESTORAN;
+            }
+            case "POSLASTICARNICA":
+            {
+                return enums.Categories.POSLASTICARNICA;
+            }
+            case "PICERIJA":
+            {
+                return enums.Categories.PICERIJA;
+            }
+        }
+
+        return null;
+    }
+
+    public Map<String, Restaurant> getAllRestaurants()
+    {
+        return allRestaurants;
     }
 
 }
